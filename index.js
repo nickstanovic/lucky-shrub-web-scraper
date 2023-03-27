335f81e6b3bcf810bae9ae62ae00553ff0385ef8
Nick Stanovic <nickstanovic@gmail.com>
2023-03-26 21:38:49 -0400

    modify downloads so that files are named after their plant

--- index.js
+++ index.js
@@ -4,74 +4,63 @@ const fs = require('fs');
 const path = require('path');
 
 
-// Function to scrape plant data from a given URL
 async function scrapePlantData(page, url) {
-    // Navigate to the provided URL
     await page.goto(url);
 
-    // Get the Image URLs
     const imageElements = await page.$$('div.field-item.odd > img');
     const plantImages = [];
     for (let i = 0; i < 5; i++) {
         plantImages.push(await imageElements[i].getProperty('src'));
     }
 
-    // Download the images to the specified location
-    const downloadDir = 'C:/Users/nicks/Downloads';
-    for (let i = 0; i < 5; i++) {
-        const imageUrl = await plantImages[i].jsonValue();
-        const imageFilename = path.basename(imageUrl);
-        const imagePath = path.join(downloadDir, imageFilename);
-        request(imageUrl).pipe(fs.createWriteStream(imagePath));
-    }
-
-    // Get the Brand
     const brandElements = await page.$$('strong');
     const brandTexts = [];
     for (let i = 0; i < 5; i++) {
         brandTexts.push(await brandElements[i].textContent());
     }
 
-    // Get the Common Name
     const commonNameElements = await page.$$('span.common-name');
     const commonNameTexts = [];
     for (let i = 0; i < 5; i++) {
         commonNameTexts.push(await commonNameElements[i].textContent());
     }
 
-    // Get the Genus
     const genusElements = await page.$$('span.genus > em');
     const genusTexts = [];
     for (let i = 0; i < 5; i++) {
         genusTexts.push(await genusElements[i].textContent());
     }
 
-// Get the Species// Get the Species
     const speciesElements = await page.$$('span.species > em');
     const speciesTexts = [];
     for (let i = 0; i < 5; i++) {
         const speciesText = await speciesElements[i].textContent();
-        // Capitalize the first letter of the species text
         const capitalizedSpeciesText = speciesText.charAt(0).toUpperCase() + speciesText.slice(1);
         speciesTexts.push(capitalizedSpeciesText);
     }
 
-    // Initialize an empty array to store the concatenated strings
-    const concatenatedStrings = [];
-
-    // Iterate through the first 5 elements of each array
+    const downloadDir = 'C:/Users/nicks/Downloads';
     for (let i = 0; i < 5; i++) {
-        // Concatenate the text contents and add them to the concatenatedStrings array
+        const imageUrl = await plantImages[i].jsonValue();
+        const brand = await brandTexts[i];
+        const commonName = await commonNameTexts[i];
+        const genus = await genusTexts[i];
+        const species = await speciesTexts[i];
+
+        const formattedFilename = `${brand}-${commonName}-${genus}-${species}.jpg`.split(' ').join('');
+        const imagePath = path.join(downloadDir, formattedFilename);
+        request(imageUrl).pipe(fs.createWriteStream(imagePath));
+    }
+
+    const concatenatedStrings = [];
+    for (let i = 0; i < 5; i++) {
         concatenatedStrings.push(`${plantImages[i]} ${brandTexts[i]} ${commonNameTexts[i]} ${genusTexts[i]} ${speciesTexts[i]}`);
     }
 
-    // Return the concatenatedStrings array
     return concatenatedStrings;
 }
 
-// Main function to handle browser initialization and scraping tasks
 async function main() {
-    // Launch a new browser with no headless mode so I can see what's happening
     const browser = await chromium.launch();
     const page = await browser.newPage();
 
